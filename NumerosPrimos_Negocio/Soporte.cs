84f5bf7d36a2ec785a898b91bf1088e4746b9f8f
Javiera Leyton <javi95lm@gmail.com>
2024-11-12 14:00:53 -0300

    Add files via upload

---
+++ NumerosPrimos_Negocio/Soporte.cs
@@ -0,0 +1,12 @@
+﻿using NumerosPrimos_Negocio;
+
+namespace NumerosPrimos_Soporte
+{
+    public class Soporte
+    {
+        public (string rango, int menor, int mayor, int cantidad, double tiempo) ObtenerResultados (string rango)
+        {
+            return Negocio.CalcularPrimos(rango);
+        }
+    }
+}
